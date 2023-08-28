e703a252294817824091dca6703501e5b3365506
Iamneo <techadmin@iamneo.ai>
2023-08-28 07:30:36 +0000

    Backup Commit

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,49 +1,26 @@
-// import { BrowserModule } from '@angular/platform-browser';
-// import { NgModule } from '@angular/core';
-
-// import { AppRoutingModule } from './app-routing.module';
-// import { AppComponent } from './app.component';
-// //import { CurrencyComponent } from './currency/currency.component';
-// import { FormsModule, ReactiveFormsModule } from '@angular/forms';  
-// import { NgxCurrencyDirective } from 'ngx-currency';
-
-// @NgModule({
-//   declarations: [
-//     AppComponent
-//     //CurrencyComponent
-//   ],
-//   imports: [
-//     BrowserModule,
-//     FormsModule,
-//     ReactiveFormsModule,
-//     AppRoutingModule,
-//     //tulasi
-//     NgxCurrencyDirective
-//   ],
-//   providers: [],
-//   bootstrap: [AppComponent]
-// })
-// export class AppModule { }
-
-
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'; // Add this line
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-//import { CurrencyConverterComponent } from './currency-converter/currency-converter.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';  
+import { NgxCurrencyDirective } from 'ngx-currency';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
+    //CurrencyComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
-    FormsModule // Add this line
+    //tulasi
+    NgxCurrencyDirective
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
